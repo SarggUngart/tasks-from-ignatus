cfb3884e421bb5ec50c31919a8e5f6fc364c96cc
Sergei Lukashin <lsv1387@gmail.com>
2022-03-15 19:38:33 +0300

    1-HW-done

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -12,10 +12,19 @@ function Message(props: MessageDataPropsType) {
   return (
     <div className={s.message}>
 
-      <img className={s.img} src={props.avatar} alt="fvatar"/>
-      <div> {props.name}</div>
-      <div> {props.message}</div>
-      <div> {props.time} </div>
+      <div className={s.wrapper}>
+
+        <img className={s.img} src={props.avatar} alt="avatar"/>
+
+        <div className={s.content__wrapper}>
+          <h4 className={s.name}> {props.name}</h4>
+          <p className={s.text}> {props.message}</p>
+          <p className={s.time}> {props.time} </p>
+          <div className={s.triangle}></div>
+          <div className={s.circle}></div>
+        </div>
+
+      </div>
 
     </div>
   )
